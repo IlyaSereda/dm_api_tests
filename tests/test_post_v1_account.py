5326140e9d58ee08c633a4d0bf1eb85a75bd4768
IlyaSereda <ilyasereda141@gmail.com>
2023-05-03 02:04:45 +0300

    Add MailBox client

--- tests/test_post_v1_account.py
+++ tests/test_post_v1_account.py
@@ -1,16 +1,18 @@
 import requests
 from services.dm_api_account import DmApiAccount
+from services.mailhog import MailhogApi
 
 
 def test_post_v1_account():
+    mailhog = MailhogApi(host='http://localhost:5025')
     api = DmApiAccount(host='http://localhost:5051')
     json = {
-        "login": "login_5",
-        "email": "login5@gmail.com",
-        "password": "login_5"
+        "login": "login_8",
+        "email": "login8@gmail.com",
+        "password": "login_8"
     }
-    response = api.account.post_v1_account(
-        json=json
-    )
-
+    response = api.account.post_v1_account(json=json)
+    token = mailhog.get_token_from_last_email()
+    response = api.account.put_v1_account_token(token=token)
     print(response)
+    print(response.json())
--- tests/test_put_v1_account_token.py
+++ tests/test_put_v1_account_token.py
@@ -1,6 +1,10 @@
 import requests
 
 
+from services.dm_api_account import DmApiAccount
 
+def test_put_v1_account_token():
+    api = DmApiAccount(host='http://localhost:5051')
+    api.account.put_v1_account_token(token='123123123')
 
 
